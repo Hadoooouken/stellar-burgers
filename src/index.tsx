d463f3ef4bae1800393e8518374aedd194051b2a
Filipp <wolverinexdd@yandex.ru>
2024-10-01 23:00:07 +0400

    deploy process.ens test

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,37 @@ import App from './components/app/app';
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
+const basename =
+  process.env.NODE_ENV === 'production' ? '/stellar-burgers' : '/';
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
+
+// import React from 'react';
+// import * as ReactDOMClient from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+
+// import store from './services/store';
+// import { Provider } from 'react-redux';
+
+// import App from './components/app/app';
+
+// const container = document.getElementById('root') as HTMLElement;
+// const root = ReactDOMClient.createRoot(container!);
+
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </React.StrictMode>
+// );
